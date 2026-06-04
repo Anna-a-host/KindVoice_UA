58925243c11810d62c6f874612da215e43cd7b3b
Anna-a-host <annachernaya2019@gmail.com>
2026-06-04 13:55:18 +0300

    Changes in main.py

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ def home():
     return "Bot is alive and running!"
 
 def run_web_server():
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000)) 
     app.run(host='0.0.0.0', port=port)
+
 
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
