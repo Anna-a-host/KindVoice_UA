d2eda46cc65515d1ed75cad48ca03e397264d7a1
Anna-a-host <annachernaya2019@gmail.com>
2026-06-06 09:09:09 +0300

    Fixing problems with voice service

--- services/voice_service.py
+++ services/voice_service.py
@@ -6,9 +6,8 @@ client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 def transcribe_audio(audio_path):
     with open(audio_path, "rb") as file: 
         translation = client.audio.transcriptions.create(
-            file=("voice.ogg", file.read()), 
+            file=("voice.ogg", file), 
             model="whisper-large-v3", 
             response_format="text"
         )
     return translation
-
